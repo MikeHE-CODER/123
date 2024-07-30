9075ae5990b88ba30324eaecce31fa5fbfc953e1
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 09:58:34 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int sum_diagonal(int array[4][4]) {
   int total = 0;
   for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      total = total + array[i][j];
-    }
+      total = total + array[i][i];   
   }
   return total;
 }
+
