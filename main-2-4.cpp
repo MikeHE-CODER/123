a72f76aa217a829fe4c34ddfe805413123bf5e16
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:23:20 +0930

    Update main-2-4.cpp

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+extern int array_min(int integers[], int length);
+extern int array_max(int integers[], int length);
 using namespace std;
 int main() {
   int a[5] = {1, 2, 6, -1, 1};
