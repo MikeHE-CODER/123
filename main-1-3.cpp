ec862aed7d0768689e336b05d11aa78cfc831df0
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 10:18:54 +0930

    Update main-1-3.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+int main() {
+    int array[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {3, 4, 6, 2}, {3, 5, 2, 6}};
+    count_digits(array);
+  
+}
