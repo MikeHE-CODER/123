604aacf65305c033c4fe99e4a2c18d5dab4b6bb6
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 10:07:32 +0930

    Create main-1-2.cpp

---
+++ main-1-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+  int array[10][10] = {{1, 2, 3, 4}, {5, 6, 7, 8},  {3, 4, 6, 2}, {3, 5, 2, 6},
+                       {3, 4, 6, 1}, {4, 5, 76, 3}, {4, 5, 6, 3}, {4, 5, 6, 3},
+                       {4, 5, 6, 3}, {4, 5, 6, 3}};
+  bool result = is_identity(array);
+}
