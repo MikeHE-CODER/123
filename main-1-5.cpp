558b10364c76cecb11e1588f5485fd838d2707c6
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:20:18 +0930

    Update main-1-5.cpp

--- main-1-5.cpp
+++ main-1-5.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+extern void print_summed(int array1[3][3],int array2[3][3]) ;
 int main() {
   int array1[3][3] = {{1, 2, 3}, {5, 7, 8}, {3, 4, 2}};
   int array2[3][3] = {{1, 2, 3}, {5, 7, 8}, {3, 4, 2}};
