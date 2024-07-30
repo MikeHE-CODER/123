07027018751c5f651c387b77e1b8710c645e7d89
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:19:03 +0930

    Update function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 void count_digits(int array[4][4]) {
-  int number = 0;
   for (int m = 0; m < 10; m++) {
     int count = 0;
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
-        if (array[i][j] == number) {
+        if (array[i][j] == m) {
           count++;
         }
       }
