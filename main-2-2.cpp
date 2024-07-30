04a4895d07d2543df687f402983ef68a3e95370d
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:20:59 +0930

    Update main-2-2.cpp

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+extern int binary_to_int(int binary_digits[], int number_of_digits);
 int main() {
   int a[5] = {1, 1, 1, 1, 1};
   cout << binary_to_int(a, 5);
