62b9e909b7d0299e7d0b9e85452d4920ac21fd92
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:17:15 +0930

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+extern bool is_identity(int array[10][10]);
 int main() {
   int array[10][10] = {{1, 2, 3, 4}, {5, 6, 7, 8},  {3, 4, 6, 2}, {3, 5, 2, 6},
                        {3, 4, 6, 1}, {4, 5, 76, 3}, {4, 5, 6, 3}, {4, 5, 6, 3},
