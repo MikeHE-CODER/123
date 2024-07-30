6df2fdef20b49ae8a1a3bf435f4913e7c6899f69
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 10:26:29 +0930

    Create main-1-5.cpp

---
+++ main-1-5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+  int array1[3][3] = {{1, 2, 3}, {5, 7, 8}, {3, 4, 2}};
+  int array2[3][3] = {{1, 2, 3}, {5, 7, 8}, {3, 4, 2}};
+  print_summed(array1,array2);
+}
