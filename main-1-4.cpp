a6866bbf18a891f1677b1c19f030a1ceb721e8a9
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:19:55 +0930

    Update main-1-4.cpp

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+extern void print_scaled(int array[3][3], int scale);
 using namespace std;
 int main() {
   int array[3][3] = {{1, 2, 3}, {5, 7, 8}, {3, 4, 2}};
