df4fd48d36adc1bbfaf10013f8d51a01be344869
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:19:25 +0930

    Update main-1-3.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+extern void count_digits(int array[4][4]);
 using namespace std;
 int main() {
     int array[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {3, 4, 6, 2}, {3, 5, 2, 6}};
