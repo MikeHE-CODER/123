acfebaf2093d0b179740bddf19aef2cdbbbbb408
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:22:28 +0930

    Update main-2-3.cpp

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
+extern int sum_array_element(int integers[], int length);
+extern bool is_palindrome(int integers[], int length);
+extern int sum_if_palindrome(int integers[], int length); 
 int main() {
   int a[5] = {1, 1, 1, 1, 1};
   cout << sum_array_element(a, 5);
