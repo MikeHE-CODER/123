3813e1d7dffbcb46f5126588d45215361731a125
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-07-30 11:25:55 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+extern int sum_diagonal(int array[4][4]);
 int main() {
   int array[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {3, 4, 6, 2}, {3, 5, 2, 6}};
   cout << sum_diagonal(array) << endl;
